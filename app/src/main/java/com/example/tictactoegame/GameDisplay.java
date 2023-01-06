8d28ab3a23bce25d1d7536a6c9b98439da70685e
alinagts <godovanetsalina@gmail.com>
2023-01-06 21:32:01 +0200

    added files

---
+++ app/src/main/java/com/example/tictactoegame/GameDisplay.java
@@ -0,0 +1,14 @@
+package com.example.tictactoegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_display);
+    }
+}
