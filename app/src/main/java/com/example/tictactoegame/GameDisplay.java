7b1c02e0879630d7b118490356aba3fc33eace57
alinagts <godovanetsalina@gmail.com>
2023-01-06 21:56:40 +0200

    added file

--- app/src/main/java/com/example/tictactoegame/GameDisplay.java
+++ app/src/main/java/com/example/tictactoegame/GameDisplay.java
@@ -2,7 +2,9 @@ package com.example.tictactoegame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class GameDisplay extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class GameDisplay extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_display);
+    }
+
+    public void playAgainButtonClick(View view) {
+
+    }
+
+    public void homeButtonClick(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
