17b6ef0a4e38303186f1a9dadf1bb6754c105954
alinagts <godovanetsalina@gmail.com>
2023-01-06 21:36:48 +0200

    game setup button functionality

--- app/src/main/java/com/example/tictactoegame/PlayerSetup.java
+++ app/src/main/java/com/example/tictactoegame/PlayerSetup.java
@@ -2,13 +2,33 @@ package com.example.tictactoegame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class PlayerSetup extends AppCompatActivity {
 
+    private EditText player1;
+    private EditText player2;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_setup);
+
+        player1 = findViewById(R.id.player1Name);
+        player1 = findViewById(R.id.player2Name);
+    }
+
+    public void submitButtonClick(View view) {
+        String player1Name = player1.getText().toString();
+        String player2Name = player1.getText().toString();
+
+        Intent intent = new Intent(this, GameDisplay.class);
+        intent.putExtra("PLAYER_NAMES", new String[] {player1Name, player2Name});
+        startActivity(intent);
     }
 }
